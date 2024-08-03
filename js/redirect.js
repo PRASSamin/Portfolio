d0cf550964cc879e55b602ec5708fb0835fc3b57
PRASSamin <prassamin@gmail.com>
2024-08-04 03:12:54 +0600

    v2.0.3

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,12 @@
 if (window.location.origin === "https://prassamin.github.io") {
   window.location.replace("https://prassamin.onrender.com/");
 }
+
+if (
+  window.location.pathname !== "/about.html" ||
+  window.location.pathname !== "/blogs.html" ||
+  window.location.pathname !== "/projects.html" ||
+  window.location.pathname !== "/index.html"
+) {
+  window.location.replace("https://prassamin.onrender.com/index.html");
+}
